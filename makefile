26dd3e806a1fa80ce374e20d5a3a7204ee34845c
i4c6026 <you@example.com>
2021-07-16 10:20:07 -0500

    write button

--- makefile
+++ makefile
@@ -1,14 +1,17 @@
 .PHONY : build clean-u clean-all run shell jup stop
 
 CONTAINER_TAG=example_name
+FOODCAST=$(shell aws s3 ls s3://ia-packages/foodcast --recursive | sort | tail -n 1 | awk '{print $$4}')
 
 help: ## 	    Commands to run
 	@fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//' | sed -e 's/##//'
 
 build: ##     Build the Docker Container
 	echo
+	echo "Download foodcast from S3."
+	aws s3 cp s3://ia-packages/$(FOODCAST) ./packages/$(FOODCAST)
 	echo "Build the Docker Container and tag it: $(CONTAINER_TAG)"
-	docker build --tag $(CONTAINER_TAG) .
+	docker build --tag $(CONTAINER_TAG) . --build-arg FC_VERSION=$(FOODCAST)
 
 clean-u: ##   Clean unused docker containers
 	echo
@@ -20,14 +23,23 @@ clean-all: ## Clean *all* docker containers
 	echo "Deleting *ALL* Docker containers!"
 	docker container stop $(docker container ls --all --quiet) && docker system prune --all --force --volumes
 
+d_base_image: ##   	Download Forecasting Base Image
+	echo
+	echo "Downloading base forecasting image"
+	aws ecr get-login-password | docker login --username AWS --password-stdin 644944822023.dkr.ecr.us-west-1.amazonaws.com/ia-forecasting-base
+	docker pull 644944822023.dkr.ecr.us-west-1.amazonaws.com/ia-forecasting-base:latest
+	docker tag 644944822023.dkr.ecr.us-west-1.amazonaws.com/ia-forecasting-base:latest forecasting_base
+
 run: ##       Run the main.py file
-	docker run --publish 8050:8050 --volume "$(CURDIR)":/container_root $(CONTAINER_TAG) python3 src/app.py
+	docker run --publish 8050:8050 --volume "$(CURDIR)":/app --env AWS_ACCESS_KEY_ID=$(AWS_ACCESS_KEY_ID) \
+    --env AWS_SECRET_ACCESS_KEY=$(AWS_SECRET_ACCESS_KEY) $(CONTAINER_TAG) \
+ 	python3 src/app.py
 
 shell: ##     Launch the container shell (for dev)
 	echo
 	echo "Opening shell in $(CONTAINER_TAG)"
 	echo "To leave the shell run: 'exit'"
-	docker run --interactive --tty --volume "$(pwd)":/container_root\
+	docker run --interactive --tty --volume "$(pwd)":/app\
     $(CONTAINER_TAG) /bin/sh
 
 jup: ##       Launch Jupyter Notebooks
--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash_table import DataTable, FormatTemplate
 import os
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-data = pd.DataFrame({'measure_1': [10, 5, 8, 4],
+from foodcast.aws import AwsConn
+
+AWS = AwsConn('ia-forecasting')
+
+data = pd.DataFrame({'measure_1': [12, 5, 8, 4],
                      'group_1': ['a', 'b'] * 2,
                      'group_2': ['c'] * 2 + ['d'] * 2})
 
@@ -14,6 +19,10 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
+    dcc.Dropdown(
+        id='group',
+        options=[{'label': ele, 'value': ele} for ele in data['group_1'].unique()]
+    ),
     dcc.RadioItems(
         id='bar-type',
         options=[
@@ -24,7 +33,18 @@ app.layout = html.Div([
     ),
     dcc.Graph(
         id='bar-graph'
-    )
+    ),
+    DataTable(
+        id='table',
+        columns=[dict(id=i, name=i, type='numeric') for i in data.columns],
+        editable=True
+    ),
+dcc.ConfirmDialogProvider(
+        children=html.Button('Save Adjustment'),
+        id='submit_button',
+        message='Are you sure you want to save your Forecast adjustment?'
+    ),
+    html.Div(id='output-provider')
 ])
 
 
@@ -35,6 +55,28 @@ def bar(bar_value):
     fig = px.bar(data, x='group_1', y='measure_1', color='group_2', barmode=bar_value)
     return fig
 
+
+@app.callback(
+    dash.dependencies.Output('table', 'data'),
+    [dash.dependencies.Input('group', 'value')]
+)
+def update_table(group_val):
+    data2 = data.copy()
+    if group_val:
+        data2 = data2.groupby('group_1').sum().reset_index()
+    return data2.to_dict('records')
+
+
+@app.callback(
+    dash.dependencies.Output('output-provider', 'children'),
+    [dash.dependencies.Input('submit_button', 'submit_n_clicks')],
+    [dash.dependencies.State('table', 'data'),
+     dash.dependencies.State('table', 'columns')])
+def update_output(submit_n_clicks, data, columns):
+    if submit_n_clicks:
+        df = pd.DataFrame(data, columns=[c['name'] for c in columns])
+        AWS.put_object_to_s3(df, key='test.csv')
+        return f"Forecast adjustments were saved! Sent {submit_n_clicks} times."
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=8050)
